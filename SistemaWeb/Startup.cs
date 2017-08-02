dacf6744e604cc5966ab3085657ca716dcfc587b
GaryGit <garyio.org@gmail.com>
2017-08-02 11:50:00 -0500

    INICIO
    Inicio del proyecto.

---
+++ SistemaWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SistemaWeb.Startup))]
+namespace SistemaWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
